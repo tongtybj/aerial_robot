f8824ef03c737978ac28a99d445f062019a7a9fb
Zhaoqi Ma <mzq0502@gamil.com>
2023-06-24 01:11:18 +0900

    [Mini Quadrotor][launch] modify ceilingavoidance.launch into jsk format

--- robots/mini_quadrotor/scripts/ceiling_avoid.py
+++ robots/mini_quadrotor/scripts/ceiling_avoid.py
@@ -17,21 +17,22 @@ class Ceilingavoid():
     self.max_height = rospy.get_param("~max_height", 9)#2.0
     self.min_height = rospy.get_param("~min_height", 0)#1.0
     self.ascend_speed = rospy.get_param("~ascend_speed", 0.01)
+    self.avoid_distance = rospy.get_param("~avoid_distance",0.1)
+    self.thresh_distance = rospy.get_param("~thresh_distance",0.2)
+    self.rate = rospy.get_param("~main_rate",1.0/20.0) #20Hz
     self.flight_nav = FlightNav()
     self.flight_nav.target = FlightNav.COG
-    self.rate = 1.0/20.0 #20Hz
-    self.ceiling_avoid_sub = rospy.Subscriber("/quadrotor/ceiling/min_distance", Float32, self.doceiling_avoidcb)
-    self.limit_distance = rospy.get_param("/distance",0.2)
+    self.ceiling_avoid_sub = rospy.Subscriber("/quadrotor/ceiling/min_distance", Float32, self.doceiling_avoidCb)
     self.flag = 0
     self.now_z = 0
     # self.now_z = None
-    self.avoid_distance = rospy.get_param("/avoid",0.1) 
+
     time.sleep(0.5)
 
-  def doceiling_avoidcb(self,msg):
+  def doceiling_avoidCb(self,msg):
     now_distance = msg.data
    # rospy.loginfo("ok!")
-    if now_distance <= 0.2:
+    if now_distance <= self.thresh_distance:
       self.flag = 2
 
   def odomCb(self, msg):
